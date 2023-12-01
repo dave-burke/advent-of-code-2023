ae1db47445f73a3b6b6cf1ede412361ba5ca03c8
Dave Burke <dburke84@gmail.com>
2023-12-01 07:43:38 -0600

    Solve Day 01 Part 01

--- solutions/day01/solution.go
+++ solutions/day01/solution.go
@@ -1,7 +1,47 @@
 package day01
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
 func Part1(input string) string {
-	return "todo"
+	lines := strings.Split(input, "\n")
+	nums := []int{}
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		numString := ""
+		for i := 0; i < len(line); i++ {
+			char := line[i]
+			if char >= 48 && char <= 57 {
+				numString += string([]byte{char})
+				break
+			}
+		}
+		for i := len(line) - 1; i >= 0; i-- {
+			char := line[i]
+			if char >= 48 && char <= 57 {
+				numString += string([]byte{char})
+				break
+			}
+		}
+		num, err := strconv.Atoi(numString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums = append(nums, num)
+		fmt.Printf("%s => %s\n", line, numString)
+	}
+	var sum int = 0
+	for i := 0; i < len(nums); i++ {
+		sum += nums[i]
+	}
+
+	return fmt.Sprint(sum)
 }
 
 func Part2(input string) string {
