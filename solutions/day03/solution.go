d9f83dd2b0f35f92d28bdc7587a464d1deeb8834
Dave Burke <dburke84@gmail.com>
2023-12-03 00:20:12 -0600

    Implement first (wrong) attempt

--- solutions/day03/solution.go
+++ solutions/day03/solution.go
@@ -1,16 +1,57 @@
 package day03
 
 import (
+	"aoc/utils/aocgrid"
 	"aoc/utils/aocinput"
 	"fmt"
+	"log"
+	"regexp"
 )
 
 func Part1() string {
-	grid := aocinput.ReadSampleAsGrid(3)
-	for _, row := range grid {
-		fmt.Println(string(row))
+	grid := aocgrid.Grid(aocinput.ReadSampleAsGrid(3))
+
+	// TODO identify strings of numbers.
+	// This line contains 467 and 114:
+	// 467..114..
+
+	nParts := 0
+	for _, point := range grid.AllPoints() {
+		r, err := grid.GetAt(point)
+		if err != nil {
+			continue
+		}
+		if isNumber(point, grid) && isPart(point, grid) {
+			fmt.Printf("%c at {%d, %d} is a part\n", r, point.Row(), point.Col())
+			nParts += int(r - '0')
+		} else {
+			fmt.Printf("%c at {%d, %d} is NOT a part\n", r, point.Row(), point.Col())
+		}
 	}
-	return "todo"
+	return fmt.Sprintf("%d", nParts)
+}
+
+func isNumber(p aocgrid.Point, g aocgrid.Grid) bool {
+	r, err := g.GetAt(p)
+	if err != nil {
+		log.Fatal(err)
+	}
+	re := regexp.MustCompile("[0-9]")
+	return re.MatchString(string(r))
+}
+
+func isPart(p aocgrid.Point, g aocgrid.Grid) bool {
+	neighbors := g.Neighbors(p)
+	for _, neighbor := range neighbors {
+		if isSymbol(neighbor) {
+			return true
+		}
+	}
+	return false
+}
+
+func isSymbol(r rune) bool {
+	return r == '+' || r == '*' || r == '$' || r == '#'
 }
 
 func Part2() string {
