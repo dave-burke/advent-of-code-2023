b6b7ebe553ac635f886886509bfb0acb6044ba9d
Dave Burke <dburke84@gmail.com>
2023-12-03 15:11:46 -0600

    Add grid_test

---
+++ utils/aocgrid/grid_test.go
@@ -0,0 +1,63 @@
+package aocgrid
+
+import "testing"
+
+var example = Grid{
+	{'.', 'O', '.'},
+	{'O', 'X', 'O'},
+	{'.', 'O', '.'},
+}
+
+func TestAll(t *testing.T) {
+	got := len(example.All())
+	wanted := 9
+
+	if got != wanted {
+		t.Errorf("Wanted %v but got %v", wanted, got)
+	}
+}
+
+type boundsTest struct {
+	point      Point
+	isInBounds bool
+}
+
+var boundsTests = []boundsTest{
+	{Point{-1, -1}, false},
+	{Point{-1, 0}, false},
+	{Point{-1, 1}, false},
+	{Point{-1, 2}, false},
+	{Point{-1, 3}, false},
+	{Point{0, -1}, false},
+	{Point{0, 0}, true},
+	{Point{0, 1}, true},
+	{Point{0, 2}, true},
+	{Point{0, 3}, false},
+	{Point{1, -1}, false},
+	{Point{1, 0}, true},
+	{Point{1, 1}, true},
+	{Point{1, 2}, true},
+	{Point{1, 3}, false},
+	{Point{2, -1}, false},
+	{Point{2, 0}, true},
+	{Point{2, 1}, true},
+	{Point{2, 2}, true},
+	{Point{2, 3}, false},
+	{Point{3, -1}, false},
+	{Point{3, 0}, false},
+	{Point{3, 1}, false},
+	{Point{3, 2}, false},
+	{Point{3, 3}, false},
+}
+
+func TestBounds(t *testing.T) {
+	for _, test := range boundsTests {
+		got := example.isInBounds(test.point)
+		wanted := test.isInBounds
+
+		if got != wanted {
+			t.Errorf("Wanted %v but got %v", wanted, got)
+		}
+	}
+
+}
