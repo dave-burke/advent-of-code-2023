3532c175b741a59170eafae93e0030288d8f924c
Dave Burke <dburke84@gmail.com>
2023-12-02 23:15:26 -0600

    Read input as grid

--- solutions/day03/solution.go
+++ solutions/day03/solution.go
@@ -1,6 +1,15 @@
 package day03
 
+import (
+	"aoc/utils/aocinput"
+	"fmt"
+)
+
 func Part1() string {
+	grid := aocinput.ReadSampleAsGrid(3)
+	for _, row := range grid {
+		fmt.Println(string(row))
+	}
 	return "todo"
 }
 
