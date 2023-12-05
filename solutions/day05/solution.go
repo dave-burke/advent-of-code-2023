0a1af022a949efad1c799b942c1dd5260ff239b7
Dave Burke <dburke84@gmail.com>
2023-12-05 05:57:49 -0600

    Add initial mapping impl

--- solutions/day05/solution.go
+++ solutions/day05/solution.go
@@ -10,6 +10,35 @@ func Part1() string {
 	return fmt.Sprint(len(lines))
 }
 
+type farmMap []farmMapping
+
+func (m farmMap) get(key int) int {
+	for _,mapping := range m {
+		if val, ok := mapping.getWithin(key); ok {
+			return val
+		}
+	}
+	return key
+}
+
+type farmMapping struct {
+	destinationRangeStart int
+	sourceRangeStart      int
+	length                int
+}
+
+func (m farmMapping) contains(key int) bool {
+	return key >= m.sourceRangeStart && key < m.sourceRangeStart+m.length
+}
+
+func (m farmMapping) getWithin(key int) int, bool {
+	if m.contains(key) {
+		dist := key - m.sourceRangeStart
+		return m.destinationRangeStart + dist, true
+	}
+	return _, false
+}
+
 func Part2() string {
 	return "todo"
 }
