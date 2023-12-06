861e9c880b0a87455e2db0dd015c95ba37382512
Dave Burke <dburke84@gmail.com>
2023-12-06 10:31:00 -0600

    Imlement part1

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	day "aoc/solutions/day05"
+	day "aoc/solutions/day06"
 	"fmt"
 )
 
 func main() {
-	result := day.Part2()
+	result := day.Part1()
 	fmt.Printf("Solution: %s\n", result)
 }
