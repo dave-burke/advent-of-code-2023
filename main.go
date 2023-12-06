44487ce58a314bf8817c1593a31bacc7a6423ec2
Dave Burke <dburke84@gmail.com>
2023-12-06 13:54:19 -0600

    Init day7

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	day "aoc/solutions/day06"
+	day "aoc/solutions/day07"
 	"fmt"
 )
 
 func main() {
-	result := day.Part2()
+	result := day.Part1()
 	fmt.Printf("Solution: %s\n", result)
 }
