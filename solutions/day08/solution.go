fc0f41b7f468f3dde69ac75d9f3a54f2cea9007a
Dave Burke <dburke84@gmail.com>
2023-12-08 07:25:42 -0600

    Implement basic input parsing

--- solutions/day08/solution.go
+++ solutions/day08/solution.go
@@ -3,11 +3,21 @@ package day08
 import (
 	"aoc/utils/aocinput"
 	"fmt"
+	"log"
+	"strings"
 )
 
 func Part1() string {
-	lines := aocinput.ReadInputAsString(8)
-	return fmt.Sprint(lines)
+	lines := aocinput.ReadSampleAsString(8)
+	headerAndBody := strings.Split(lines, "\n\n")
+	header := headerAndBody[0]
+	body := headerAndBody[1]
+
+	bodyLines := strings.Split(body, "\n")
+
+	log.Print(header)
+	log.Printf("%v", bodyLines)
+	return fmt.Sprint(len(bodyLines))
 }
 
 func Part2() string {
