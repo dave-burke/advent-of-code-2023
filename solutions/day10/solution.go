636ab8b42141f739860f89e24176aca6ee020d6d
Dave Burke <dburke84@gmail.com>
2023-12-09 00:17:21 -0600

    Init day10

---
+++ solutions/day10/solution.go
@@ -0,0 +1,20 @@
+package day10
+
+import (
+	"aoc/utils/aocinput"
+	"fmt"
+)
+
+func Part1() string {
+	lines := aocinput.ReadSampleAsLines(10)
+	// lines := aocinput.ReadInputAsLines(10)
+
+	return fmt.Sprint(len(lines))
+}
+
+func Part2() string {
+	lines := aocinput.ReadSampleAsLines(10)
+	// lines := aocinput.ReadInputAsLines(10)
+
+	return fmt.Sprint(len(lines))
+}
